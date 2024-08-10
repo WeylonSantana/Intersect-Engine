74bbaa7a4198a5bc7bfcd41f4e4a8c2bf8c380a2
WeylonSantana <weylonsantanaoff@gmail.com>
2024-08-10 11:28:11 -0300

    add/remove user and logging who is changing the presence list

--- Intersect.Server/CustomChange/SCFVHub/SCFVHub.cs
+++ Intersect.Server/CustomChange/SCFVHub/SCFVHub.cs
@@ -3,6 +3,8 @@
 using Intersect.CustomChange;
 using Intersect.CustomChange.SCFVHub;
 using Intersect.Enums;
+using Intersect.Logging;
+using Intersect.Server.Core;
 using Intersect.Server.Database;
 using Microsoft.AspNetCore.SignalR;
 using Newtonsoft.Json;
@@ -12,6 +14,8 @@ namespace Intersect.Server.CustomChange.SCFVHub;
 public class SCFVHub : Hub
 {
     private static readonly object _lock = new();
+
+    private Logger _logger => ServerContext.Instance.Logger;
 
     public override async Task OnConnectedAsync()
     {
@@ -23,19 +27,20 @@ public class SCFVHub : Hub
         await base.OnDisconnectedAsync(exception);
     }
 
+    // This method is called by the client to get the current state of the event
     public SCFVConfig GetConfig()
     {
         if (!Directory.Exists("resources-custom"))
         {
             _ = Directory.CreateDirectory("resources-custom");
-            SaveConfig(new SCFVConfig());
+            _saveConfig(new SCFVConfig());
         }
 
         var json = File.ReadAllText("resources-custom/config.json");
         return JsonConvert.DeserializeObject<SCFVConfig>(json) ?? new SCFVConfig();
     }
 
-    public void SaveConfig(SCFVConfig config)
+    private void _saveConfig(SCFVConfig config)
     {
         File.WriteAllText(
             "resources-custom/config.json",
@@ -46,24 +51,31 @@ public class SCFVHub : Hub
         );
     }
 
+    // This method is called by the client to get the current state of the event
     public List<string> GetUserList(SCFVUserType type)
     {
         if (!Directory.Exists("resources-custom"))
         {
             _ = Directory.CreateDirectory("resources-custom");
-            File.WriteAllText(
-                $"resources-custom/users-{type.ToString().ToLowerInvariant()}.json",
-                JsonConvert.SerializeObject(
-                    new List<SCFVUser>(),
-                    Formatting.Indented
-                )
-            );
+            _saveUserList(type, new List<string>());
         }
 
         var json = File.ReadAllText($"resources-custom/users-{type.ToString().ToLowerInvariant()}.json");
         return JsonConvert.DeserializeObject<List<string>>(json) ?? [];
     }
 
+    private void _saveUserList(SCFVUserType type, List<string> userList)
+    {
+        File.WriteAllText(
+            $"resources-custom/users-{type.ToString().ToLowerInvariant()}.json",
+            JsonConvert.SerializeObject(
+                userList.Distinct().ToList(),
+                Formatting.Indented
+            )
+        );
+    }
+
+    // This method is called by the client to get the current presence list
     public List<SCFVUser> GetPresenceList(long? timestamp)
     {
         var date = timestamp.HasValue ? DateTimeOffset.FromUnixTimeMilliseconds(timestamp.Value) : DateTimeOffset.Now;
@@ -73,16 +85,10 @@ public class SCFVHub : Hub
         return presence?.PresenceList ?? [];
     }
 
+    // This method is called by the client to update the presence list
     public async Task UpdatePresenceList(string workerId, string username, bool isAdding)
     {
         var presence = SCFVPresenceBase.GetPresenceByName();
-
-        // If the user is already in the list, don't add them again
-        if (presence.PresenceList.Any(u => u.Name == username) == isAdding)
-        {
-            await Clients.All.SendAsync("PresenceListUpdated", presence.PresenceList);
-            return;
-        }
 
         lock (_lock)
         {
@@ -102,10 +108,13 @@ public class SCFVHub : Hub
                         LastModified = DateTimeOffset.Now.ToString("yyyy-MM-dd HH:mm:ss")
                     }
                 );
+
+                _logger.Info($"The worker {workerId} has added {username} to the presence list.");
             }
             else
             {
                 _ = presence.PresenceList.RemoveAll(u => u.Name == username);
+                _logger.Info($"The worker {workerId} has removed {username} from the presence list.");
             }
 
             presence.PresenceList = presence.PresenceList.Distinct().ToList();
@@ -115,11 +124,42 @@ public class SCFVHub : Hub
         await Clients.All.SendAsync("update");
     }
 
+    // This method is called by the admin to toggle the event
     public async Task ToggleEvent()
     {
         var config = GetConfig();
         config.IsOpen = !config.IsOpen;
-        SaveConfig(config);
+        _saveConfig(config);
+
+        await Clients.All.SendAsync("update");
+    }
+
+    // This method is called by the admin to add a user to the event
+    public async Task AddUser(string username)
+    {
+        var userList = GetUserList(SCFVUserType.Night);
+        if (string.IsNullOrWhiteSpace(username) || userList.Contains(username))
+        {
+            return;
+        }
+
+        userList.Add(username);
+        _saveUserList(SCFVUserType.Night, userList);
+
+        await Clients.All.SendAsync("update");
+    }
+
+    // This method is called by the admin to remove a user from the event
+    public async Task RemoveUser(string username)
+    {
+        var userList = GetUserList(SCFVUserType.Night);
+        if (string.IsNullOrWhiteSpace(username) || !userList.Contains(username))
+        {
+            return;
+        }
+
+        _ = userList.Remove(username);
+        _saveUserList(SCFVUserType.Night, userList);
 
         await Clients.All.SendAsync("update");
     }
